28f28b01af214102887146e044f89c4db8d6901f
Charlotte Soneson <charlottesoneson@gmail.com>
2021-05-17 08:24:29 +0200

    Add some packages

--- pkg_install.R
+++ pkg_install.R
@@ -1,4 +1,7 @@
 (mirror <- getOption("repos"))
 
-install.packages("units", repos = "http://cran.rstudio.com/")
+install.packages("BiocManager", repos = "http://cran.rstudio.com/")
+BiocManager::install(c("units", "RSQLite", "sp", "shiny", 
+                       "gdtools", "rgeos", "sf"), 
+                     update = FALSE, dependencies = TRUE)
 saveRDS(NULL, file = "pkg_install.rds")
