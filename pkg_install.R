8aea32ef5d52ccaa433be82c554be9e1c8300942
Charlotte Soneson <charlottesoneson@gmail.com>
2021-05-17 10:17:02 +0200

    Try with larger set of packages again

--- pkg_install.R
+++ pkg_install.R
@@ -2,9 +2,8 @@
 .libPaths()
 
 install.packages("BiocManager", repos = "http://cran.rstudio.com/")
-pkgs <- c("units")
-# pkgs <- c("units", "RSQLite", "sp", "shiny", 
-#           "gdtools", "rgeos", "sf")
+pkgs <- c("units", "RSQLite", "sp", "shiny",
+          "gdtools", "rgeos", "sf")
 BiocManager::install(pkgs, 
                      update = FALSE, dependencies = TRUE)
 saveRDS(NULL, file = "pkg_install.rds")
